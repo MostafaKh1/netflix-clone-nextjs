2dc33eedeb175f80338c8471c4dafbbc96ac6e33
MostafaKh1 <chiiit1754@gmail.com>
2022-10-08 20:32:33 +0200

    1/3 finish homepage

---
+++ components/Header.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
+import { BsSearch } from "react-icons/bs";
+import { BsBellFill } from "react-icons/bs";
+import { useEffect, useState } from "react";
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"} py-0 `}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image
+          src="/Netflix-logo.png"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="header-link">Home</li>
+          <li className="header-link">TV Show</li>
+          <li className="header-link">Movies</li>
+          <li className="header-link">New & Popular</li>
+          <li className="header-link">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center  space-x-4  text-sm font-light ">
+        <BsSearch className="hidden sm:inline " size={32} />
+        <p className="hidden lg:inline">Kids</p>
+        <BsBellFill size={32} />
+        <Link href="/account">
+          <img className="w-8 " src="/Netflix-avatar.png " />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ components/Thumbnail.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Movie } from "../typing";
+import Image from "next/image";
+interface Props {
+  movie: Movie;
+}
+export default function Thumbnail({ movie }: Props) {
+  return (
+    <div className="relative h-28 min-w-[180px] cursor-pointer duration-200 ease-out md:h-36 md:min-w-[260px md:hover: scale-105]">
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+      />
+    </div>
+  );
+}
